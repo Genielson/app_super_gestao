4f599093abb49c6267e20d79956a5dc7a6274fb3
Genielson <ge.nielson@hotmail.com>
2022-05-03 19:44:52 -0300

    Criando controllers SobreNosController e ContatoController

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/','PrincipalController@principal');
+
+
+Route::get('/sobre-nos', function () {
+    return '';
+});
+
+
+Route::get('/contato', function () {
+    return '';
 });
